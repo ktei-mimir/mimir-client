b1640833ef45e9b37deb8fd405e658ee0602c307
ktei <ktei2008@gmail.com>
2023-11-12 20:27:55 +1100

    continue to fix socket

--- src/context/WebSocketContext.tsx
+++ src/context/WebSocketContext.tsx
@@ -1,6 +1,6 @@
 import { contextFactory } from '@/helpers/contextFactory'
 
-import { ReactNode, useEffect, useMemo, useState } from 'react'
+import { ReactNode, useCallback, useMemo, useState } from 'react'
 import useWebSocket from 'react-use-websocket'
 import { useAuth0 } from '@auth0/auth0-react'
 import { WebSocketLike } from 'react-use-websocket/dist/lib/types'
@@ -21,33 +21,47 @@ export { useWebSocketContext }
 const WebSocketContextProvider = (props: Props) => {
   const { children } = props
   const [state, setState] = useState<WebSocketValues>({ socket: null })
-  const [socketUrl, setSocketUrl] = useState<string>(
-    'wss://9riswt00ti.execute-api.ap-southeast-2.amazonaws.com/prod/'
-  )
+  // const [socketUrl, setSocketUrl] = useState<string>(
+  //   'wss://9riswt00ti.execute-api.ap-southeast-2.amazonaws.com/prod/'
+  // )
 
   const { getAccessTokenSilently } = useAuth0()
 
-  const { getWebSocket } = useWebSocket(socketUrl)
+  // In functional React component
+  const getSocketUrl = useCallback(async () => {
+    const token = await getAccessTokenSilently()
+    return `wss://9riswt00ti.execute-api.ap-southeast-2.amazonaws.com/prod/?token=${token}`
+  }, [getAccessTokenSilently])
+
+  const { getWebSocket } = useWebSocket(getSocketUrl, {
+    onOpen: () => {
+      console.log('websocket connected')
+      setState({
+        socket: getWebSocket()
+      })
+    },
+    shouldReconnect: () => true,
+    reconnectAttempts: 10,
+    //attemptNumber will be 0 the first time it attempts to reconnect, so this equation results in a reconnect pattern of 1 second, 2 seconds, 4 seconds, 8 seconds, and then caps at 10 seconds until the maximum number of attempts is reached
+    reconnectInterval: attemptNumber =>
+      Math.min(Math.pow(2, attemptNumber) * 1000, 10000)
+  })
 
   // const handleSocketOpen = useCallback(() => {
   //   console.log('socket opened')
   // }, [])
 
-  useEffect(() => {
-    getAccessTokenSilently().then(token => {
-      const socketURL = `wss://9riswt00ti.execute-api.ap-southeast-2.amazonaws.com/prod/?token=${token}`
-      setSocketUrl(socketURL)
-      // const ws = new WebSocket(`${socketURL}?token=${token}`)
-      setState({
-        socket: getWebSocket()
-      })
-    })
-
-    // Close ws connection when component unmounts
-    // return () => {
-    //   state.socket?.close()
-    // }
-  }, [getAccessTokenSilently, getWebSocket, socketUrl])
+  // useEffect(() => {
+  //   setState({
+  //     socket: getWebSocket()
+  //   })
+  // }, [getWebSocket])
+  //
+  //   // Close ws connection when component unmounts
+  //   // return () => {
+  //   //   state.socket?.close()
+  //   // }
+  // }, [getAccessTokenSilently, getWebSocket, socketUrl])
 
   const values = useMemo(() => state ?? {}, [state])
   return (
